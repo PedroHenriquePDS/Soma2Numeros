345dfdef3fdbe1cae560eba64f1fe73bc2165e7e
Pedro Henrique <PedroHenrique053010@gmail.com>
2022-09-29 22:04:23 -0300

    Adiciona a resolução ao exercicio Soma2numeros

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int numeroUm, numeroDois, resultado;
+
+Console.WriteLine("Cálculo da soma entre dois números.\n");
+
+Console.Write("Digite o primeiro número: ");
+numeroUm = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+numeroDois = Convert.ToInt32(Console.ReadLine());
+
+resultado = numeroUm + numeroDois;
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"\nSoma: {resultado}");
+Console.ResetColor();
+
+Console.WriteLine("");
+
+Console.WriteLine("Presione uma tecla para sair...");
+Console.ReadKey();
